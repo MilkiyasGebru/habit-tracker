87a4a45cad671ebb53c19aca8f9105e4df66abf4
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-27 20:02:56 +0300

    Added A select option for the area in adding habit

--- components/AddHabit.jsx
+++ components/AddHabit.jsx
@@ -1,18 +1,36 @@
 "use client"
 
-import {CgGym} from "react-icons/cg";
+import {CgArrowDown, CgArrowUp} from "react-icons/cg";
 import IconsTooltip from "@/components/IconsTooltip";
 import {useState} from "react";
 import create_icons from "@/utils/icons_util";
+
+const area_buttons = ["Health","Gym","Food"]
 
 export default function AddHabit() {
 
     const [habitName, setHabitName] = useState("");
     const [toolTip, setToolTip] = useState(true);
     const [iconName, setIconName] = useState("gym");
+    const [areas, setAreas] = useState([]);
+    const [displayAreas, setDisplayAreas] = useState(false)
+
+
+    const handleActionButtonClick = (area)=>{
+        console.log("area is ",area)
+        if (areas.includes(area)){
+            setAreas(areas.filter(areas_element => areas_element !== area));
+            console.log(areas.filter(areas_element => areas_element !== area));
+        }
+        else{
+            setAreas([...areas,area]);
+            console.log([...areas,area]);
+        }
+    }
+
 
     return (
-        <div className="flex flex-col p-8 w-3/5 gap-6 border rounded-md">
+        <div className="flex flex-col px-8 pt-8 pb-12 w-3/5 gap-6 border rounded-md">
             {/*Title*/}
             <h1 className="text-2xl font-bold ">Add New Habit</h1>
 
@@ -60,12 +78,38 @@ export default function AddHabit() {
             </div>
 
         {/*    Areas*/}
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-3 relative z-10">
                 <h2 className="text-lg font-semibold">Areas</h2>
+
+                <div>
+
+
+                    <div className="px-4  py-2 flex items-center justify-between  border rounded h-12 " onClick={()=>{setDisplayAreas(!displayAreas)}}>
+                        <div className="flex gap-x-2">
+                            {areas.map((field,index) => {
+
+                                return <div className="py-1 w-fit border px-2 rounded-md" key={index}>
+                                    {field}
+                                </div>
+                            })}
+                        </div>
+                        {!displayAreas && <CgArrowDown className=""/>}
+                        {displayAreas && <CgArrowUp className=""/>}
+
+                    </div >
+                        <div className={`flex flex-col items-start border rounded shadow-md absolute w-full bg-white ${displayAreas? "":"hidden"}`}>
+                            {area_buttons.map((area_button_name,index) =>
+                            <button className="px-4 py-1  w-full text-left hover:bg-gray-100" key={index} onClick={(e)=>{handleActionButtonClick(area_button_name)}}>
+                                {area_button_name}
+                            </button>)}
+
+
+                    </div>
+                </div>
             </div>
 
-        {/*    Button*/}
-            <button className="px-3 py-2 rounded-md border w-1/4">
+            {/*    Button*/}
+            <button className="px-3 py-2 rounded-md border w-1/4 relative z-0">
                 Add a Habit
             </button>
 
