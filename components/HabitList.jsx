3348c22f4d476d9a826aee882197265071bb13bf
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-28 11:32:32 +0300

    Added Padding,Border, and Border Color

--- components/HabitList.jsx
+++ components/HabitList.jsx
@@ -7,32 +7,35 @@ export default function HabitList() {
     const current_date = new Date();
     const array = [1,2,3,4]
     return (
-        <div className="flex flex-col bg-white gap-4">
+        <div className="flex flex-col bg-white gap-4 p-4 border rounded-md shadow-md border-gray-100">
             <div className="flex justify-between items-center">
 
-                <div className="flex flex-col">
-                    <span className="text-xl font-semibold">Friday</span>
-                    <span>14 June 2024</span>
+                <div className="flex items-center gap-x-4">
+                    <div className="flex flex-col">
+                        <span className="text-xl font-semibold">Friday</span>
+                        <span>14 June 2024</span>
+                    </div>
+
+                    <div className="flex gap-x-1">
+                        <div className="px-1 py-1 border rounded-full flex items-center">
+                            <CgArrowLeft className="  "/>
+                        </div>
+                        <div className="px-1 py-1 border rounded-full flex items-center">
+                            <CgArrowRight/>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="flex gap-x-1">
-                    <div className="px-2 py-1 border rounded-full flex items-center">
-                        <CgArrowLeft className="  " />
-                    </div>
-                    <div className="px-2 py-1 border rounded-full flex items-center">
-                        <CgArrowRight />
-                    </div>
-                </div>
 
                 <button className="px-2 py-1 flex items-center gap-x-2 bg-blue-400 border text-gray-100 rounded-md">
-                    <CgMathPlus />
+                    <CgMathPlus/>
                     <span>New Habit</span>
                 </button>
 
             </div>
             {array.map(item => (
                 <Habit key={item}/>
-                ))}
+            ))}
         </div>
     )
 }
