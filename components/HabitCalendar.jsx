0580f413b19ce7dbf9619f3c6bf80164c46f617b
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-28 16:28:35 +0300

    Made the date a context

--- components/HabitCalendar.jsx
+++ components/HabitCalendar.jsx
@@ -1,15 +1,18 @@
 "use client"
 
-import {useState} from "react";
+import {useContext, useState} from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
+import {DateContext} from "@/context/DateContext";
 
 
 export default function HabitCalendar() {
-    const [calendar_date, setCalendarDate] = useState(new Date());
+    const {calendar_date,setCalendarDate} = useContext(DateContext)
     return (
-        <div className="w-1/2 ">
-            <Calendar onChange={setCalendarDate} value={calendar_date}/>
+        <div className="">
+            <Calendar onChange={(val, event)=>{
+                console.log("Clicked",val)
+                setCalendarDate(val)}} value={calendar_date}/>
         </div>
     )
 }
