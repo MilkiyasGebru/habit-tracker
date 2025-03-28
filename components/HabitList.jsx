6fb73fc67d68d8fed993457b8d2eaf2da8aa9089
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-28 11:22:25 +0300

    Made the arrows in a circle

--- components/HabitList.jsx
+++ components/HabitList.jsx
@@ -1,30 +1,38 @@
 import {CgArrowLeft, CgArrowRight,CgMathPlus} from "react-icons/cg";
-
+import Habit from "@/components/Habit";
+// ArrowLeft
 
 
 export default function HabitList() {
     const current_date = new Date();
-
+    const array = [1,2,3,4]
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col bg-white gap-4">
             <div className="flex justify-between items-center">
 
                 <div className="flex flex-col">
-                    <span>Friday</span>
+                    <span className="text-xl font-semibold">Friday</span>
                     <span>14 June 2024</span>
                 </div>
 
                 <div className="flex gap-x-1">
-                    <CgArrowLeft />
-                    <CgArrowRight />
+                    <div className="px-2 py-1 border rounded-full flex items-center">
+                        <CgArrowLeft className="  " />
+                    </div>
+                    <div className="px-2 py-1 border rounded-full flex items-center">
+                        <CgArrowRight />
+                    </div>
                 </div>
 
-                <button className="px-2 py-1 flex items-center gap-x-2">
+                <button className="px-2 py-1 flex items-center gap-x-2 bg-blue-400 border text-gray-100 rounded-md">
                     <CgMathPlus />
                     <span>New Habit</span>
                 </button>
 
             </div>
+            {array.map(item => (
+                <Habit key={item}/>
+                ))}
         </div>
     )
 }
