a2a533dc5552e739050e008ef61e4013b5e281aa
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-28 16:29:17 +0300

    Added a functions to the button that would add or substract 1 day

--- components/HabitList.jsx
+++ components/HabitList.jsx
@@ -1,5 +1,9 @@
 import {CgArrowLeft, CgArrowRight,CgMathPlus} from "react-icons/cg";
 import Habit from "@/components/Habit";
+import {useContext} from "react";
+import {DateContext} from "@/context/DateContext";
+import {get_day_name, get_month_name} from "@/utils/date_util";
+
 // ArrowLeft
 
 const data = {
@@ -22,26 +26,26 @@ const unfiltered_datas = [
     },]
 
 export default function HabitList({topic, search_title}) {
-    const current_date = new Date();
-    const array = [1,2,3,4]
 
+    const {calendar_date, setCalendarDate} = useContext(DateContext)
     let datas = unfiltered_datas.filter(data=> topic==="" || data.topics.includes(topic))
     datas = datas.filter(data => search_title ==="" || data.title.includes(search_title))
+
     return (
         <div className="flex flex-col bg-white gap-4 p-4 border rounded-md shadow-md border-gray-100">
             <div className="flex justify-between items-center">
 
                 <div className="flex items-center gap-x-4">
                     <div className="flex flex-col">
-                        <span className="text-xl font-semibold">Friday</span>
-                        <span>14 June 2024</span>
+                        <span className="text-xl font-semibold">{get_day_name(calendar_date)}</span>
+                        <span>{calendar_date.getDate()} {get_month_name(calendar_date)}, {calendar_date.getFullYear()} </span>
                     </div>
 
                     <div className="flex gap-x-1">
-                        <div className="px-1 py-1 border rounded-full flex items-center">
-                            <CgArrowLeft className="  "/>
+                        <div className="px-1 py-1 border rounded-full flex items-center cursor-pointer" onClick={()=>{setCalendarDate(new Date(calendar_date.getTime() - 24*60*60*1000))}}>
+                            <CgArrowLeft className=""/>
                         </div>
-                        <div className="px-1 py-1 border rounded-full flex items-center">
+                        <div className="px-1 py-1 border rounded-full flex items-center cursor-pointer" onClick={()=>{setCalendarDate(new Date(calendar_date.getTime() + 24*60*60*1000))}}>
                             <CgArrowRight/>
                         </div>
                     </div>
