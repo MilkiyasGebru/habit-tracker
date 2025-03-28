20f6aa11264364ac2e17ff485be51e5bb464b667
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-28 12:45:08 +0300

    Added a function that handle change in search title

--- components/SearchArea.jsx
+++ components/SearchArea.jsx
@@ -1,7 +1,5 @@
 import {CgSearch} from "react-icons/cg";
-
-
-export default function SearchArea(){
+export default function SearchArea({handleSearch,search_title}){
 
     return (
         <div className="flex items-center justify-between border p-4 rounded-md bg-white border-gray-100">
@@ -11,7 +9,7 @@ export default function SearchArea(){
             </div>
             <div className="border px-3 py-2 flex gap-2 items-center rounded-2xl bg-[#f7f7f7] border-gray-100">
                 <CgSearch />
-                <input type="text" className="bg-transparent outline-none" placeholder="Search..."/>
+                <input type="text" className="bg-transparent outline-none" placeholder="Search..." value={search_title} onChange={(e)=>handleSearch(e.target.value)}/>
             </div>
         </div>
     )
