26aa66b4e9537b1c2719744f6fbb6c7c0100a0b4
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-30 21:32:41 +0300

    Added more Habit Topics

--- components/HabitTopics.jsx
+++ components/HabitTopics.jsx
@@ -1,4 +1,4 @@
-import {CgAbstract, CgAdidas, CgAirplane, CgAlarm, CgGym} from "react-icons/cg";
+import {CgAbstract, CgAdidas, CgAirplane, CgAlarm, CgBorderAll, CgGym,CgHeart} from "react-icons/cg";
 import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
 
 const another_icons = [
@@ -6,16 +6,18 @@ const another_icons = [
     ["alarm",CgAlarm],
     ["airplane",CgAirplane],
     ["abstract",CgAbstract],
-    ["adidas",CgAdidas]
+    ["adidas",CgAdidas],
+    ["Health",CgHeart],
 ]
 
-export default function HabitTopics() {
+export default function HabitTopics({handleChangeTopic,current_topic}) {
     return (
-        <div className="flex items-center px-3 py-2 border rounded-md">
+        <div className="flex items-center px-3 py-2 border rounded-md bg-white border-gray-100 font-thin">
+
             {another_icons.map((icon_list,index)=>
-                <div key={index} className="flex gap-x-1 px-2 py-1 items-center text-xl">
+                <div key={index} className={`flex gap-x-1 px-2 py-1 items-center ${current_topic === index? "border rounded-md bg-blue-500 text-white":""} cursor-pointer`} onClick={()=>handleChangeTopic(index)}>
                     {icon_list[1]()}
-                    <span>{icon_list[0]}</span>
+                    <span className="font-thin">{icon_list[0]}</span>
                 </div>
             )}
         </div>
